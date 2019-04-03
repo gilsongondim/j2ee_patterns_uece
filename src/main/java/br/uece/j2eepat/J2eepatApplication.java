e27a7153a025a3a7253bec2dd8106bfbe01e2c16
kartmatias <kartmatias@gmail.com>
2019-04-03 20:40:13 -0300

    initial commit

---
+++ src/main/java/br/uece/j2eepat/J2eepatApplication.java
@@ -0,0 +1,13 @@
+package br.uece.j2eepat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eepatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2eepatApplication.class, args);
+	}
+
+}
