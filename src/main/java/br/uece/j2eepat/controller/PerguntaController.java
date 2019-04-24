431937b938f73087ee7037a159591045900bff35
kartmatias <kartmatias@gmail.com>
2019-04-24 18:50:21 -0300

    contribuicao

--- src/main/java/br/uece/j2eepat/controller/PerguntaController.java
+++ src/main/java/br/uece/j2eepat/controller/PerguntaController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.uece.j2eepat.model.Pergunta;
 import br.uece.j2eepat.service.PerguntaService;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 
 @RestController
@@ -25,17 +29,17 @@ public class PerguntaController {
     @Autowired
     private PerguntaService perguntaService;
 
-    @GetMapping("/perguntas")
+    @GetMapping("/apipergunta")
     public Page<Pergunta> getperguntas(Pageable pageable){
         return perguntaService.findAll(pageable);
     }
 
-    @PostMapping("/perguntas")
+    @PostMapping("/apipergunta")
     public Pergunta createpergunta(@Valid @RequestBody Pergunta pergunta){
         return perguntaService.save(pergunta);
     }
 
-    @PutMapping("/perguntas/{perguntaId}")
+    @PutMapping("/apipergunta/{perguntaId}")
     public Pergunta updatepergunta(@PathVariable Long perguntaId,
                                    @Valid @RequestBody Pergunta perguntaRequest){
         return perguntaService.findOne(perguntaId)
@@ -46,7 +50,7 @@ public class PerguntaController {
                 }).orElseThrow(() -> new ResourceNotFoundException("Pergunta nao encontrada com id: " + perguntaId));
     }
 
-    @DeleteMapping("/perguntas/{perguntaId}")
+    @DeleteMapping("/apipergunta/{perguntaId}")
     public ResponseEntity<?> deletepergunta(@PathVariable Long perguntaId) {
         return perguntaService.findOne(perguntaId)
                 .map(pergunta -> {
@@ -54,4 +58,59 @@ public class PerguntaController {
                     return ResponseEntity.ok().build();
                 }).orElseThrow(() -> new ResourceNotFoundException("Pergunta nao encontrada com id: " + perguntaId));
     }
+
+
+    @GetMapping("/pergunta")
+    public ModelAndView findAll() {
+        ModelAndView mv = new ModelAndView("/pergunta");
+        mv.addObject("perguntas", perguntaService.findAllVTO());
+        return mv;
+    }
+
+    @GetMapping("/perguntaAdd")
+    public ModelAndView add(Pergunta pergunta) {
+
+        ModelAndView mv = new ModelAndView("/perguntaAdd");
+        mv.addObject("pergunta", pergunta);
+
+        return mv;
+    }
+
+
+    @GetMapping("/perguntaAdd2")
+    public ModelAndView add2(Optional<Pergunta> optional) {
+
+        ModelAndView mv = new ModelAndView("/perguntaAdd");
+        mv.addObject("pergunta", optional);
+
+        return mv;
+    }
+
+    @GetMapping("/editpergunta/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+
+        return add2(perguntaService.findOne(id));
+    }
+
+    @GetMapping("/deletepergunta/{id}")
+    public ModelAndView delete(@PathVariable("id") Long id) {
+
+        perguntaService.delete(id);
+
+        return findAll();
+    }
+
+
+    @PostMapping("/savepergunta")
+    public ModelAndView save(@Valid Pergunta pergunta, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return add(pergunta);
+        }
+
+        perguntaService.save(pergunta);
+
+        return findAll();
+    }
+
 }
--- src/main/java/br/uece/j2eepat/service/PerguntaService.java
+++ src/main/java/br/uece/j2eepat/service/PerguntaService.java
@@ -3,6 +3,7 @@ package br.uece.j2eepat.service;
 import java.util.List;
 import java.util.Optional;
 
+import br.uece.j2eepat.vto.PerguntaVTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,11 +12,20 @@ import org.springframework.stereotype.Service;
 import br.uece.j2eepat.model.Pergunta;
 import br.uece.j2eepat.repository.PerguntaRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
 @Service
 public class PerguntaService {
     @Autowired
     private PerguntaRepository repository;
-     
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public List<Pergunta> findAll() {
         return repository.findAll();
     }
@@ -39,5 +49,20 @@ public class PerguntaService {
     public boolean existsById(Long id) {
     	return repository.existsById(id);
     }
+
+    public List<PerguntaVTO> findAllVTO() {
+
+        try {
+            StringBuilder sql = new StringBuilder();
+            sql.append(" SELECT NEW br.uece.j2eepat.vto.AlunoVTO(a.id, a.nome, a.numeroMatricula) ");
+            sql.append(" FROM Aluno a ");
+            Query query = entityManager.createQuery(sql.toString());
+            return query.getResultList();
+
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
 
 }
